23f1fc493108c8c59ca126dcf30f0e160587d91b
mirage335 <spamfreemirage335@gmail.com>
2014-01-21 20:31:34 +0000

    Improved build scripts.

--- build.sh
+++ build.sh
@@ -3,35 +3,47 @@
 
 scriptAbsoluteFolder="$(getScriptAbsoluteFolder)"	#Start in script's own location.
 
-#Improves readability.
-#Depends on colormake (https://github.com/pagekite/Colormake) and unbuffer (http://expect.sourceforge.net/) .
+#Improves make readability.
+#Depends on colout (http://nojhan.github.io/colout/) and unbuffer (http://expect.sourceforge.net/) .
 readableMake() {
 
-	if [[ -e $(which colormake) && -e $(which unbuffer) ]]
+	if [[ -e $(which colout) && -e $(which unbuffer) ]]
 	then
-		unbuffer colormake -j6 2>&1 | grep --color=always -E '^|undefined reference'
+		unbuffer make -j6 2>&1 | colout -t cmake | grep --color=always -E '^|undefined reference'
 	else
 		make -j6
 	fi
 }
 
+#Improves cmake readability.
+#Depends on colout (http://nojhan.github.io/colout/) .
+readableCMake() {
+	if [[ -e $(which colout) && -e $(which unbuffer) ]]
+	then 
+		cmake "$@" | colout -t cmake
+	else
+		cmake "$@"
+	fi
+}
+
+
 #Produces out-of-source build in Release folder.
 cd "$scriptAbsoluteFolder"
 mkdir -p ./Release
 cd ./Release
-cmake -DCMAKE_BUILD_TYPE=Release ../
+readableCMake -DCMAKE_BUILD_TYPE=Release ../
 readableMake
 
 #Produces out-of-source build in Debug folder.
 cd "$scriptAbsoluteFolder"
 mkdir -p ./Debug
 cd ./Debug
-cmake -DCMAKE_BUILD_TYPE=Debug ../
+readableCMake -DCMAKE_BUILD_TYPE=Debug ../
 readableMake
 
 #Produces out-of-source build in Pedantic folder.
 cd "$scriptAbsoluteFolder"
 mkdir -p ./Pedantic
 cd ./Pedantic
-cmake -DCMAKE_BUILD_TYPE=Pedantic ../
+readableCMake -DCMAKE_BUILD_TYPE=Pedantic ../
 readableMake
--- cmakeOnly.sh
+++ cmakeOnly.sh
@@ -3,20 +3,31 @@
 
 scriptAbsoluteFolder="$(getScriptAbsoluteFolder)"	#Start in script's own location.
 
+#Improves cmake readability.
+#Depends on colout (http://nojhan.github.io/colout/) .
+readableCMake() {
+	if [[ -e $(which colout) && -e $(which unbuffer) ]]
+	then 
+		cmake "$@" | colout -t cmake
+	else
+		cmake "$@"
+	fi
+}
+
 #Produces out-of-source build in Release folder.
 cd "$scriptAbsoluteFolder"
 mkdir -p ./Release
 cd ./Release
-cmake -DCMAKE_BUILD_TYPE=Release ../
+readableCMake -DCMAKE_BUILD_TYPE=Release ../
 
 #Produces out-of-source build in Debug folder.
 cd "$scriptAbsoluteFolder"
 mkdir -p ./Debug
 cd ./Debug
-cmake -DCMAKE_BUILD_TYPE=Debug ../
+readableCMake -DCMAKE_BUILD_TYPE=Debug ../
 
 #Produces out-of-source build in Pedantic folder.
 cd "$scriptAbsoluteFolder"
 mkdir -p ./Pedantic
 cd ./Pedantic
-cmake -DCMAKE_BUILD_TYPE=Pedantic ../
+readableCMake -DCMAKE_BUILD_TYPE=Pedantic ../
