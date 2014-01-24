850d4fb78d80a6cf030c0b834572b6a1d9e02fb4
mirage335 <spamfreemirage335@gmail.com>
2014-01-24 23:32:37 +0000

    Repaired README.

--- readLog.sh
+++ readLog.sh
@@ -1,31 +1,3 @@
 #!/bin/bash
 
-#Retrieves absolute path of current script, while maintaining symlinks, even when "./" would translate with "readlink -f" 
-#Suitable for allowing scripts to find other scripts they depend on.
-getScriptAbsoluteLocation() {                                       
-        local absoluteLocation                                                                                           
-        if [[ (-e $PWD\/$0) && ($0 != "") ]]                                                                             
-                        then                                                                                             
-        absoluteLocation="$PWD"\/"$0"                                                                                    
-        absoluteLocation=$(realpath -s "$absoluteLocation")         
-                        else                               
-        absoluteLocation="$0"                              
-        fi                                  
-                                                           
-        if [[ -h "$absoluteLocation" ]]                                                                                  
-                        then                                        
-        absoluteLocation=$(readlink -f "$absoluteLocation")                                                              
-        fi                                                                                                               
-                                                                    
-        echo $absoluteLocation                                                                                           
-}
-
-#Retrieves absolute path of current script, while maintaining symlinks, even when "./" would translate with "readlink -f" 
-#Suitable for allowing scripts to find other scripts they depend on.                                                      
-getScriptAbsoluteFolder() {                                                                                               
-        dirname "$(getScriptAbsoluteLocation)"                                                                            
-}
-
-cd "$(getScriptAbsoluteFolder)"	#Start in script's own location.
-
-cat ./dataLog | baudline -stdin -format le32f -samplerate 150
+cat "$1" | baudline -stdin -format le32f -samplerate 150
